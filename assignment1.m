7881c3e480366645c2f95cf0a05f05b3f081b4ef
filippogandolfi <didi.gandolfi@gmail.com>
2019-10-17 18:03:22 +0200

    task 3

--- assignment1.m
+++ assignment1.m
@@ -1,16 +1,37 @@
 %% Filippo Gandolfi
+% Robotics Engineering - II year - 2019/2020
+% Machine Learning - Assignment 1 
 
 % Upload data
-% !!trovare forma migliore per applicarlo sempre!!
+% This works only with a pre modified file.
+% It's possible to implemente a script that automatically change the file.
+
 load('weatherdata.mat');
 weather_data = table2array(weatherdata);
 [n, d] = size(weather_data);
 
-% Split into training and test
+% Split into training and test 
 index = randperm(n);
-trset = weather_data(index(1:10), :);
-teset = weather_data(index(11:end), 1:(d-1));
-target = weather_data(index(11:end),d);
+trset = weather_data(index(1:10), :); % dataset for training 
+teset = weather_data(index(11:end), 1:(d-1)); % dataset for test
+
+target = weather_data(index(11:end),d); %this is used for calculate the error rate
+ 
+% For TASK 3, I create a new training set
+    for i=1:d
+        max_init_val(i) = max(weather_data(:,i)); % max value present in each column
+    end
+
+trset3 = [max_init_val;trset]; %add a row at the top
+% testWrong = [2,2,2,2,2;2,2,2,2,2;2,2,2,2,2;2,2,2,2,2;2,2,2,2,2;2,2,2,2,2;];
+% trset3 = testWrong;
 
 % Calling the Naive Bayes classifier
 [result, e_rate] = naiveBayes(trset, teset, target);
+
+[final_prob3,e_rate3] = task3(trset3,max_init_val,teset,target);
+
+    
+    
+    
+    
--- task3.m
+++ task3.m
@@ -1,5 +1,5 @@
-function [prediction, e_rate] = naiveBayes(trset,teset,target)
-    [n, d] = size(trset);
+function [final_prob3,e_rate3] = task3(trset3,max_init_val,teset,target)
+    [n, d] = size(trset3);
     [m, f] = size(teset);
     
     %check size of the matrices
@@ -15,7 +15,7 @@ function [prediction, e_rate] = naiveBayes(trset,teset,target)
     %check training set 
     for i=1:n
         for j = 1:d 
-            if trset(i,j) < 1
+            if trset3(i,j) < 1
                error('error value');                
             end
         end
@@ -30,26 +30,29 @@ function [prediction, e_rate] = naiveBayes(trset,teset,target)
         end
     end  %%
 
-    e_rate = 0;
+    e_rate3 = 0;    
+    a = 1;
+    n3 = max_init_val; % is the number of values of attribute x
+    
     %% First task: Training
     % I need the probability of each event
     for i=1:d
-        max_val(i) = max(trset(:,i)); % max value present in each column
+        max_val(i) = max(trset3(:,i)); % max value present in each column
         for j = 1:max_val 
-            prob_classes(j,i) = sum(trset(:,i)==j); % how many times the value appear 
+            prob_classes(j,i) = sum(trset3(:,i)==j); % how many times the value appear 
             prob_val(j,i) = prob_classes(j,i)/n; % pounded by the number of rows
         end
     end
     
     num_classes = max_val(d); % Number of classes present
     
-    prob_tot=zeros(max(max_val),d-1,num_classes);
+    prob_tot3=zeros(max(max_val),d-1,num_classes);
     for i=1:(d-1)
         for j=1:n
             for p=1:max_val(i)
                 for k=1:num_classes
-                    if (trset(j,i)==p && trset(j,d)==k)
-                        prob_tot(p,i,k) = prob_tot(p,i,k)+1; % create N (num_classes) matrices with them probability for every value
+                    if (trset3(j,i)==p && trset3(j,d)==k)
+                        prob_tot3(p,i,k) = prob_tot3(p,i,k)+1; % create N (num_classes) matrices with them probability for every value
                     end
                 end
             end
@@ -60,7 +63,7 @@ function [prediction, e_rate] = naiveBayes(trset,teset,target)
     for i=1:d-1
         for j=1:max(max_val)
             for k=1:num_classes
-                prob_tot(j,i,k) = prob_tot(j,i,k)/prob_classes(k,d); % pounded by the number of classes
+                prob_tot3(j,i,k) = (prob_tot3(j,i,k)+a)/(prob_classes(k,d)+(a*n3(j))); % pounded by the number of classes
             end
         end
     end
@@ -72,9 +75,9 @@ function [prediction, e_rate] = naiveBayes(trset,teset,target)
       for i=1:m
         for j=1:f
               val = teset(j,i); % used for findind the values in the test and match with the one trained
-              prob_situa (i,k) = prob_situa (i,k) * prob_tot(val,i,k); % prob for each row
+              prob_situa (i,k) = prob_situa (i,k) * prob_tot3(val,i,k); % prob for each row
         end
-          final_prob = prob_situa * prob_classes(k,d); % applying the Bayes formula
+          final_prob3 = prob_situa * prob_classes(k,d); % applying the Bayes formula
       end
   end
   
@@ -82,7 +85,7 @@ function [prediction, e_rate] = naiveBayes(trset,teset,target)
   norm_prob = zeros(m,num_classes);
   for i=1:m
       for j=1:num_classes
-          norm_prob(i,j) = final_prob(i,j) / sum(final_prob(i,:));
+          norm_prob(i,j) = final_prob3(i,j) / sum(final_prob3(i,:));
       end
   end
   
@@ -93,15 +96,19 @@ function [prediction, e_rate] = naiveBayes(trset,teset,target)
               
           if (prediction(i) == target(i))  
           else
-            e_rate = (e_rate+1);
+            e_rate3 = (e_rate3+1);
           end % error rate
       end
-      e_rate = (e_rate/m)*100;
+      e_rate3 = (e_rate3/m)*100;
   end                  
-          
-%esempio lezione
-%
-%
-%
-%
-%
+    
+%     
+%     for i=1:d-1
+%         for j=1:max(max_val)
+%             for k=1:num_classes
+%                 prob_task3(j,i,k) = (prob_task3(j,i,k)+a)/(prob_classes(k,d)+(a*n3(j))); % pounded by the number of classes
+%             end
+%         end
+%     end      
+%   % end of the training 
+% end
